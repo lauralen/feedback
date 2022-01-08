44ae1fcc444acb1b4f020382ee34b77dcd7cb05a
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-01-08 12:01:57 +0200

    Set up SCSS theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.scss'
 
 function App() {
-  return <div className="App">App</div>
+  return (
+    <div>
+      <div className="theme-light">
+        <div className="App">Light themed</div>
+      </div>
+
+      <div className="theme-dark">
+        <div className="App">Dark themed</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
