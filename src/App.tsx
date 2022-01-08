d9defe0f3458faaf98179dc14b85fa6eb1248b1c
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-01-08 12:25:38 +0200

    Add toggle theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
+import cx from 'classnames'
+
 import './App.scss'
 
+type Theme = 'light' | 'dark'
+
 function App() {
+  const [theme, setTheme] = useState<Theme>('light')
+
   return (
     <div>
-      <div className="theme-light">
-        <div className="App">Light themed</div>
+      <div
+        className={cx({
+          'theme-light': theme === 'light',
+          'theme-dark': theme === 'dark',
+        })}
+      >
+        <div className="App">App</div>
       </div>
 
-      <div className="theme-dark">
-        <div className="App">Dark themed</div>
-      </div>
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+        Toggle theme
+      </button>
     </div>
   )
 }
