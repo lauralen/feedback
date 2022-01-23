8bceb3c266928bf585071bee0af9686f3e492e76
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-01-23 19:05:51 +0200

    Style dark mode

--- src/layout/Content/Content.tsx
+++ src/layout/Content/Content.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
 const Content: FC = ({ children }) => {
-  return <div className="content">{children}</div>
+  return (
+    <div className="content">
+      <div className="width-limiter">{children}</div>
+    </div>
+  )
 }
 
 export default Content
