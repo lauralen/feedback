65b60f9b58d480da9923bf712b00e0c7fccb04c1
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-01-25 21:14:05 +0200

    Update Countries test

--- src/features/countries/Countries.test.tsx
+++ src/features/countries/Countries.test.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
-import { screen, waitFor } from '@testing-library/react'
+import { getByText, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { server } from 'mocks/server'
 import { graphql } from 'msw'
@@ -17,19 +17,27 @@ const Component = (
 
 describe('Countries', () => {
   it('renders correctly', async () => {
-    const { getByText, queryByText, getAllByText, getByPlaceholderText } =
-      render(Component)
+    render(Component)
 
-    expect(getByText(/countries/i)).toBeInTheDocument()
-    expect(getByText(/loading/i)).toBeInTheDocument()
+    expect(screen.getByText(/countries/i)).toBeInTheDocument()
+    expect(screen.getByText(/loading/i)).toBeInTheDocument()
 
-    await waitFor(() => expect(queryByText(/loading/i)).not.toBeInTheDocument())
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+    )
 
-    expect(queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
 
-    expect(getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER)).toBeInTheDocument()
-    expect(getAllByText(/andorra/i)).toHaveLength(2)
-    expect(getByText(/united arab emirates/i)).toBeInTheDocument()
+    const REGION_SELECT = screen.getByLabelText(/Filter by Region/i)
+    expect(screen.getByText(/countries/i)).toBeInTheDocument()
+    expect(
+      screen.getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER)
+    ).toBeInTheDocument()
+    expect(REGION_SELECT).toBeInTheDocument()
+    expect(REGION_SELECT).toHaveValue('None')
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+    expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
   })
 
   it('shows error message if countries request fails', async () => {
@@ -38,11 +46,19 @@ describe('Countries', () => {
         return res(ctx.status(500))
       })
     )
+    render(Component)
 
-    const { getByText, queryByText } = render(Component)
-
-    await waitFor(() => expect(queryByText(/loading/i)).not.toBeInTheDocument())
-    expect(getByText(/error/i)).toBeInTheDocument()
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+    )
+    expect(screen.getByText(/error/i)).toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByPlaceholderText(SEARCH_INPUT_PLACEHOLDER)
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByPlaceholderText(/Filter by Region/i)
+    ).not.toBeInTheDocument()
   })
 
   it('filters countries by search value', async () => {
@@ -53,15 +69,157 @@ describe('Countries', () => {
       expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
     )
 
-    userEvent.type(
-      screen.getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER),
-      'unit'
-    )
+    const searchInput = screen.getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER)
+
+    userEvent.type(searchInput, 'unit')
     expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
     expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
 
-    userEvent.clear(screen.getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER))
+    userEvent.clear(searchInput)
     expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
-    await waitFor(() => expect(screen.getAllByText(/andorra/i)).toHaveLength(2))
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+
+    userEvent.type(searchInput, 'me')
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/no countries match/i)).toBeInTheDocument()
+    userEvent.clear(searchInput)
+  })
+
+  it('renders region select', async () => {
+    render(Component)
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument()
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+    )
+
+    const defaultOption = screen.getByRole('option', {
+      name: /none/i,
+    }) as HTMLOptionElement
+    const asiaOption = screen.getByRole('option', {
+      name: /asia/i,
+    }) as HTMLOptionElement
+    const africaOption = screen.getByRole('option', {
+      name: /africa/i,
+    }) as HTMLOptionElement
+    const northAmericaOption = screen.getByRole('option', {
+      name: /north america/i,
+    }) as HTMLOptionElement
+    const southAmericaOption = screen.getByRole('option', {
+      name: /south america/i,
+    }) as HTMLOptionElement
+    const europeOption = screen.getByRole('option', {
+      name: /europe/i,
+    }) as HTMLOptionElement
+    const oceaniaOption = screen.getByRole('option', {
+      name: /oceania/i,
+    }) as HTMLOptionElement
+
+    expect(screen.getByLabelText(/filter by region/i)).toBeInTheDocument()
+    expect(defaultOption.selected).toBe(true)
+    expect(asiaOption.selected).toBe(false)
+    expect(africaOption.selected).toBe(false)
+    expect(northAmericaOption.selected).toBe(false)
+    expect(southAmericaOption.selected).toBe(false)
+    expect(europeOption.selected).toBe(false)
+    expect(oceaniaOption.selected).toBe(false)
+  })
+
+  it('handles filtering by region', async () => {
+    render(Component)
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument()
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+    )
+
+    const regionSelect = screen.getByLabelText(/filter by region/i)
+    const asiaOption = screen.getByRole('option', {
+      name: /asia/i,
+    }) as HTMLOptionElement
+    const noneOption = screen.getByRole('option', {
+      name: /none/i,
+    }) as HTMLOptionElement
+    const europeOption = screen.getByRole('option', {
+      name: /europe/i,
+    }) as HTMLOptionElement
+
+    userEvent.selectOptions(regionSelect, asiaOption)
+    expect(asiaOption.selected).toBe(true)
+    expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
+    expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+
+    userEvent.selectOptions(regionSelect, noneOption)
+    expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+
+    userEvent.selectOptions(regionSelect, europeOption)
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+  })
+
+  it('handles filtering by search and selected region', async () => {
+    render(Component)
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument()
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+    )
+
+    const searchInput = screen.getByPlaceholderText(SEARCH_INPUT_PLACEHOLDER)
+    const regionSelect = screen.getByLabelText(/filter by region/i)
+    const europeOption = screen.getByRole('option', {
+      name: /europe/i,
+    }) as HTMLOptionElement
+    const asiaOption = screen.getByRole('option', {
+      name: /asia/i,
+    }) as HTMLOptionElement
+
+    userEvent.selectOptions(regionSelect, europeOption)
+    expect(europeOption.selected).toBe(true)
+
+    userEvent.type(searchInput, 'and')
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+
+    userEvent.type(searchInput, '1')
+    expect(screen.getByText(/no countries match/i)).toBeInTheDocument()
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
+
+    userEvent.clear(searchInput)
+    userEvent.type(searchInput, 'an')
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.getAllByText(/andorra/i)).toHaveLength(2)
+
+    userEvent.clear(searchInput)
+    userEvent.type(searchInput, 'un')
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/no countries match/i)).toBeInTheDocument()
+    expect(screen.queryByText(/united arab emirates/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
+
+    userEvent.selectOptions(regionSelect, asiaOption)
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/no countries match/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/united arab emirates/i)).toBeInTheDocument()
+    expect(screen.queryByText(/andorra/i)).not.toBeInTheDocument()
   })
 })
