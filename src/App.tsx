002a67f4d6e41e1849133afe786d5cf357fb01eb
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-03 18:22:24 +0300

    update react version, install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,22 @@
-import { useState } from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import cx from 'classnames'
-import Content from 'layout/Content'
-import Header from 'layout/Header'
 
 import Button from 'common/components/Button'
 import Countries from 'features/countries/Countries'
 import Country from 'features/countries/Country'
 
-type Theme = 'light' | 'dark'
-
 function App() {
-  const [theme, setTheme] = useState<Theme>('light')
-
   return (
     <BrowserRouter>
-      <div
-        className={cx('app', {
-          'theme-light': theme === 'light',
-          'theme-dark': theme === 'dark',
-        })}
-      >
-        <div className="app">
-          <Header>
-            <Link to="/">
-              <h1>Where in the world?</h1>
-            </Link>
-            <Button
-              onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-            >
-              Toggle theme
-            </Button>
-          </Header>
+      <div>
+        <Link to="/">
+          <h1>Where in the world?</h1>
+        </Link>
+        <Button onClick={() => {}}>Toggle theme</Button>
 
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Content>
-                  <Countries />
-                </Content>
-              }
-            />
-            <Route
-              path="/country/:code"
-              element={
-                <Content>
-                  <Country />
-                </Content>
-              }
-            />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route path="/country/:code" element={<Country />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { store } from './app/store'
 import App from './App'
@@ -14,7 +15,9 @@ if (process.env.REACT_APP_ENABLE_MOCKS) {
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
