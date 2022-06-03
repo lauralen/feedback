765abb49c9d4f51377028e9a07f759611a444d5c
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-03 21:59:42 +0300

    clean up code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 
-import countriesReducer from '../features/countries/countriesSlice'
+// import countriesReducer from '../features/countries/countriesSlice'
 
 export const store = configureStore({
   reducer: {
-    countries: countriesReducer,
+    // countries: countriesReducer,
   },
 })
 
