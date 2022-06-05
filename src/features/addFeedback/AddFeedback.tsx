c8b760f2f70be584bae61af55cd7a8b11515a721
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-05 14:27:52 +0300

    add arrow icon to go back

--- src/features/addFeedback/AddFeedback.tsx
+++ src/features/addFeedback/AddFeedback.tsx
@@ -1,10 +1,18 @@
+import { Box } from '@chakra-ui/react'
+import { ReactComponent as LeftArrowIcon } from 'assets/icons/icon-arrow-left.svg'
+
 import Card from 'common/components/Card'
 import Link from 'common/components/Link'
 
 function Feedbacks() {
   return (
     <>
-      <Link to="/"> Go back</Link>
+      <Link to="/" display="flex" alignItems="center">
+        <LeftArrowIcon />
+        <Box as="span" ml="3">
+          Go back
+        </Box>
+      </Link>
       <Card></Card>
     </>
   )
