93ef81ffe67e673ac084b0c5ea6a0bb371775cbf
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-05 19:11:17 +0300

    use formik for form

--- src/features/addFeedback/AddFeedback.tsx
+++ src/features/addFeedback/AddFeedback.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Box, Flex } from '@chakra-ui/react'
 import { ReactComponent as PlusIcon } from 'assets/icons/icon-new-feedback.svg'
+import { Field, Formik } from 'formik'
 
 import Button from 'common/components/Button'
 import Card from 'common/components/Card'
@@ -19,40 +20,71 @@ function Feedbacks() {
       <GoBackLink />
       <Card icon={<PlusIcon />} mt="12" py="10" px="6">
         <H1 mb="6">Create New Feedback</H1>
-        <form>
-          <FormControl>
-            <FormLabel htmlFor="title">Feedback Title</FormLabel>
-            <FormHelper>Add a short, descriptive headline</FormHelper>
-            <Input id="title" />
-          </FormControl>
+        <Formik
+          initialValues={{ title: '', category: '', details: '' }}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2))
+              actions.setSubmitting(false)
+            }, 1000)
+          }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <Field name="title">
+                {({ field }) => (
+                  <FormControl>
+                    <FormLabel htmlFor="title">Feedback Title</FormLabel>
+                    <FormHelper>Add a short, descriptive headline</FormHelper>
+                    <Input {...field} id="title" />
+                  </FormControl>
+                )}
+              </Field>
 
-          <FormControl>
-            <FormLabel htmlFor="category">Category</FormLabel>
-            <FormHelper>Choose a category for your feedback</FormHelper>
-            <Select
-              id="category"
-              options={[{ value: 'feature', label: 'Feature' }]}
-            />
-          </FormControl>
+              <Field name="category">
+                {({ field }) => (
+                  <FormControl>
+                    <FormLabel htmlFor="category">Category</FormLabel>
+                    <FormHelper>Choose a category for your feedback</FormHelper>
+                    <Select
+                      {...field}
+                      onChange={(e) => field.onChange(e)}
+                      id="category"
+                      options={[
+                        { value: 'feature', label: 'Feature' },
+                        { value: 'feature2', label: 'Feature2' },
+                      ]}
+                    />
+                  </FormControl>
+                )}
+              </Field>
 
-          <FormControl>
-            <FormLabel htmlFor="details">Feedback Detail</FormLabel>
-            <FormHelper>
-              Include any specific comments on what should be improved, added,
-              etc.
-            </FormHelper>
-            <TextArea id="details" />
-          </FormControl>
+              <Field name="details">
+                {({ field }) => (
+                  <FormControl>
+                    <FormLabel htmlFor="details">Feedback Detail</FormLabel>
+                    <FormHelper>
+                      Include any specific comments on what should be improved,
+                      added, etc.
+                    </FormHelper>
+                    <TextArea {...field} id="details" />
+                  </FormControl>
+                )}
+              </Field>
 
-          <Flex mt="10" direction="column" align="stretch">
-            <Button mb="4">Add Feedback</Button>
-            <Link to="/">
-              <Button variant="secondary" w="100%">
-                Cancel
-              </Button>
-            </Link>
-          </Flex>
-        </form>
+              <Flex mt="10" direction="column" align="stretch">
+                <Button mb="4" type="submit">
+                  Add Feedback
+                </Button>
+                <Link to="/">
+                  <Button variant="secondary" w="100%">
+                    Cancel
+                  </Button>
+                </Link>
+              </Flex>
+            </form>
+          )}
+        </Formik>
       </Card>
     </Box>
   )
