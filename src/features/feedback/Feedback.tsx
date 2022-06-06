0e4df8567ce91d684c51a30cb9487e26b78cf78a
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-06 19:47:51 +0300

    use formik for post comment form

--- src/features/feedback/Feedback.tsx
+++ src/features/feedback/Feedback.tsx
@@ -1,10 +1,20 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Center, Flex, Image, List } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Flex,
+  FormErrorMessage,
+  Image,
+  List,
+  useToast,
+} from '@chakra-ui/react'
+import { Field, Formik, FormikHelpers } from 'formik'
 
 import Button from 'common/components/Button'
 import Card from 'common/components/Card'
 import FeedbackCard from 'common/components/FeedbackCard'
+import FormControl from 'common/components/FormControl'
 import GoBackLink from 'common/components/GoBackLink'
 import H1 from 'common/components/H1'
 import H2 from 'common/components/H2'
@@ -15,8 +25,16 @@ import { Feedback, Status } from 'common/types'
 
 import { fetchRequest } from './api'
 
+const COMMENT_CHAR_LIMIT = 250
+
+const getRemainingCharactersCount = (value: string, limit: number): number => {
+  const result = limit - value.length
+  return result > 0 ? result : 0
+}
+
 function Feedbacks() {
-  let { id } = useParams()
+  const toast = useToast()
+  const { id } = useParams()
 
   const [status, setStatus] = useState<Status>('loading')
   const [data, setData] = useState<Feedback>()
@@ -40,6 +58,34 @@ function Feedbacks() {
       }
     })()
   }, [id])
+
+  const validateComment = (value: string): string | undefined => {
+    if (!value) {
+      return "Can't be empty"
+    } else if (value.length > COMMENT_CHAR_LIMIT) {
+      return `Can't be longer than ${COMMENT_CHAR_LIMIT} characters`
+    }
+  }
+
+  const onSubmit = async (values: any, actions: FormikHelpers<any>) => {
+    try {
+      // await postRequest(values)
+
+      toast({
+        title: 'Comment posted successfully',
+        status: 'success',
+        isClosable: true,
+      })
+    } catch {
+      toast({
+        title: 'Failed to post comment',
+        status: 'error',
+        isClosable: true,
+      })
+    } finally {
+      actions.setSubmitting(false)
+    }
+  }
 
   return (
     <>
@@ -108,11 +154,52 @@ function Feedbacks() {
 
                 <Card p="6">
                   <H1 mb="6">Add Comment</H1>
-                  <TextArea mb="4" placeholder="Type your comment here" />
-                  <Flex align="center" justify="space-between">
-                    <Text>250 Characters left</Text>
-                    <Button>Post Comment</Button>
-                  </Flex>
+                  <Formik
+                    initialValues={{
+                      comment: '',
+                    }}
+                    onSubmit={onSubmit}
+                  >
+                    {({
+                      handleSubmit,
+                      errors,
+                      touched,
+                      isSubmitting,
+                      values,
+                    }) => (
+                      <form onSubmit={handleSubmit}>
+                        <Field name="comment" validate={validateComment}>
+                          {({ field }) => (
+                            <FormControl
+                              isInvalid={!!(errors.comment && touched.comment)}
+                            >
+                              <TextArea
+                                {...field}
+                                aria-label="comment"
+                                name="comment"
+                                mb="4"
+                                placeholder="Type your comment here"
+                              />
+                              <FormErrorMessage>
+                                {errors.comment}
+                              </FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+
+                        <Flex align="center" justify="space-between">
+                          <Text>
+                            {getRemainingCharactersCount(
+                              values.comment,
+                              COMMENT_CHAR_LIMIT
+                            )}{' '}
+                            Characters left
+                          </Text>
+                          <Button isLoading={isSubmitting}>Post Comment</Button>
+                        </Flex>
+                      </form>
+                    )}
+                  </Formik>
                 </Card>
               </>
             ),
