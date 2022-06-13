8026a864fbccbb70be295095ad933ce7c10f09f3
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-06-13 18:36:19 +0300

    add storybook

---
+++ src/common/components/Button/Button.stories.tsx
@@ -0,0 +1,44 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import theme from 'theme'
+
+import Button from './Button'
+
+export default {
+  title: 'Feedback/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <ChakraProvider theme={theme}>
+    <Button {...args} />
+  </ChakraProvider>
+)
+
+const commonArgs = {
+  children: 'Button',
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  ...commonArgs,
+  variant: 'primary',
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  ...commonArgs,
+  variant: 'secondary',
+}
+
+export const Blue = Template.bind({})
+Blue.args = {
+  ...commonArgs,
+  variant: 'blue',
+}
+
+export const Transparent = Template.bind({})
+Transparent.args = {
+  ...commonArgs,
+  variant: 'transparent',
+}
