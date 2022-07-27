f914ee0ec76fcc4cab2ec64d80c4049f61373540
lauralen <laurallenkutyte@gmail.com>
2022-07-27 21:11:47 +0300

    Create CommentForm component

---
+++ src/common/components/CommentForm/CommentForm.tsx
@@ -0,0 +1,106 @@
+import { FC } from 'react'
+import { Flex, FormErrorMessage, useToast } from '@chakra-ui/react'
+import { Field, Formik, FormikHelpers } from 'formik'
+
+import Button from 'common/components/Button'
+import FormControl from 'common/components/FormControl'
+import Text from 'common/components/Text'
+import TextArea from 'common/components/TextArea'
+import { postComment } from 'features/feedback/api'
+
+type Props = {
+  id?: string
+}
+
+type FormValues = { comment: string }
+
+const COMMENT_CHAR_LIMIT = 250
+
+const getRemainingCharactersCount = (value: string, limit: number): number => {
+  const result = limit - value.length
+  return result > 0 ? result : 0
+}
+
+const CommentForm: FC<Props> = ({ id }) => {
+  const toast = useToast()
+
+  const validateComment = (value: string): string | undefined => {
+    if (!value) {
+      return "Can't be empty"
+    } else if (value.length > COMMENT_CHAR_LIMIT) {
+      return `Can't be longer than ${COMMENT_CHAR_LIMIT} characters`
+    }
+  }
+
+  const onSubmit = async (
+    values: FormValues,
+    actions: FormikHelpers<FormValues>
+  ) => {
+    try {
+      if (id) {
+        await postComment({
+          ...values,
+          feedbackId: id,
+          username: 'you',
+        })
+
+        toast({
+          title: 'Comment posted successfully',
+          status: 'success',
+          isClosable: true,
+        })
+        actions.resetForm()
+      } else {
+        throw new Error('feedback id not found')
+      }
+    } catch {
+      toast({
+        title: 'Failed to post comment',
+        status: 'error',
+        isClosable: true,
+      })
+    } finally {
+      actions.setSubmitting(false)
+    }
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        comment: '',
+      }}
+      onSubmit={onSubmit}
+    >
+      {({ handleSubmit, errors, touched, isSubmitting, values }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="comment" validate={validateComment}>
+            {({ field }) => (
+              <FormControl isInvalid={!!(errors.comment && touched.comment)}>
+                <TextArea
+                  {...field}
+                  aria-label="comment"
+                  name="comment"
+                  mb="4"
+                  placeholder="Type your comment here"
+                />
+                <FormErrorMessage>{errors.comment}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+          <Flex align="center" justify="space-between">
+            <Text>
+              {getRemainingCharactersCount(values.comment, COMMENT_CHAR_LIMIT)}{' '}
+              Characters left
+            </Text>
+            <Button type="submit" isLoading={isSubmitting}>
+              Post Comment
+            </Button>
+          </Flex>
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+export default CommentForm
--- src/features/feedback/Feedback.tsx
+++ src/features/feedback/Feedback.tsx
@@ -1,40 +1,20 @@
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import {
-  Box,
-  Center,
-  Flex,
-  FormErrorMessage,
-  List,
-  useToast,
-} from '@chakra-ui/react'
-import { Field, Formik, FormikHelpers } from 'formik'
+import { Box, Center, Flex, List } from '@chakra-ui/react'
 
 import Button from 'common/components/Button'
 import Card from 'common/components/Card'
 import Comment from 'common/components/Comment'
+import CommentForm from 'common/components/CommentForm'
 import FeedbackCard from 'common/components/FeedbackCard'
-import FormControl from 'common/components/FormControl'
 import GoBackLink from 'common/components/GoBackLink'
 import H1 from 'common/components/H1'
 import Spinner from 'common/components/Spinner'
-import Text from 'common/components/Text'
-import TextArea from 'common/components/TextArea'
 import { Feedback, Status } from 'common/types'
 
-import { fetchRequest, postComment } from './api'
-
-type FormValues = { comment: string }
-
-const COMMENT_CHAR_LIMIT = 250
-
-const getRemainingCharactersCount = (value: string, limit: number): number => {
-  const result = limit - value.length
-  return result > 0 ? result : 0
-}
+import { fetchRequest } from './api'
 
 function Feedbacks() {
-  const toast = useToast()
   const { id } = useParams()
 
   const [status, setStatus] = useState<Status>('loading')
@@ -59,46 +39,6 @@ function Feedbacks() {
       }
     })()
   }, [id])
-
-  const validateComment = (value: string): string | undefined => {
-    if (!value) {
-      return "Can't be empty"
-    } else if (value.length > COMMENT_CHAR_LIMIT) {
-      return `Can't be longer than ${COMMENT_CHAR_LIMIT} characters`
-    }
-  }
-
-  const onSubmit = async (
-    values: FormValues,
-    actions: FormikHelpers<FormValues>
-  ) => {
-    try {
-      if (id) {
-        await postComment({
-          ...values,
-          feedbackId: id,
-          username: 'you',
-        })
-
-        toast({
-          title: 'Comment posted successfully',
-          status: 'success',
-          isClosable: true,
-        })
-        actions.resetForm()
-      } else {
-        throw new Error('feedback id not found')
-      }
-    } catch {
-      toast({
-        title: 'Failed to post comment',
-        status: 'error',
-        isClosable: true,
-      })
-    } finally {
-      actions.setSubmitting(false)
-    }
-  }
 
   return (
     <>
@@ -138,54 +78,7 @@ function Feedbacks() {
 
                 <Card p="6">
                   <H1 mb="6">Add Comment</H1>
-                  <Formik
-                    initialValues={{
-                      comment: '',
-                    }}
-                    onSubmit={onSubmit}
-                  >
-                    {({
-                      handleSubmit,
-                      errors,
-                      touched,
-                      isSubmitting,
-                      values,
-                    }) => (
-                      <form onSubmit={handleSubmit}>
-                        <Field name="comment" validate={validateComment}>
-                          {({ field }) => (
-                            <FormControl
-                              isInvalid={!!(errors.comment && touched.comment)}
-                            >
-                              <TextArea
-                                {...field}
-                                aria-label="comment"
-                                name="comment"
-                                mb="4"
-                                placeholder="Type your comment here"
-                              />
-                              <FormErrorMessage>
-                                {errors.comment}
-                              </FormErrorMessage>
-                            </FormControl>
-                          )}
-                        </Field>
-
-                        <Flex align="center" justify="space-between">
-                          <Text>
-                            {getRemainingCharactersCount(
-                              values.comment,
-                              COMMENT_CHAR_LIMIT
-                            )}{' '}
-                            Characters left
-                          </Text>
-                          <Button type="submit" isLoading={isSubmitting}>
-                            Post Comment
-                          </Button>
-                        </Flex>
-                      </form>
-                    )}
-                  </Formik>
+                  <CommentForm id={id} />
                 </Card>
               </>
             ),
